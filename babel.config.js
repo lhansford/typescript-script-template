c03de40e2a1d6ce93d082518018cdf71805db019
Luke Hansford <mail@lukehansford.me>
2020-04-13 08:24:38 +0200

    Fix jest setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, modules: false }],
+    ['@babel/preset-env', { targets: { node: 'current' }, modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false }],
     '@babel/typescript',
   ],
   plugins: ['@babel/proposal-object-rest-spread', '@babel/plugin-transform-async-to-generator'],
